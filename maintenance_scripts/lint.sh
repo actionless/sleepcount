2f7596208bfaac34e138d4748d14e75706dae2f2
actionless <actionless.loveless@gmail.com>
2024-07-24 06:49:40 +0200

    feat(maintenance_scripts: lint): add more base checks

--- maintenance_scripts/lint.sh
+++ maintenance_scripts/lint.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-target="sleepcount.py"
+TARGETS=(
+	"sleepcount.py"
+)
+PYTHON=python3
+TARGET_MODULE='sleepcount'
+
+echo -e "\n== Running python compile:"
+"$PYTHON" -O -m compileall "${TARGETS[@]}" \
+| (\
+	grep -v -e '^Listing' -e '^Compiling' || true \
+)
+echo ':: python compile passed ::'
+
+echo -e "\n== Running python import:"
+"$PYTHON" -c "from ${TARGET_MODULE} import cli"
+echo -e ':: python import passed ::\n'
 
 echo Flake8:
-flake8 "$target"
+flake8 "${TARGETS[@]}"
 
 echo PyLint:
-pylint "$target" --score no
+pylint "${TARGETS[@]}" --score no
 
 echo MyPy:
-python -m mypy "$target"
+python -m mypy "${TARGETS[@]}"
 
 	#./maintenance_scripts/vulture_whitelist.py \
 echo Vulture:
-vulture "$target" \
+vulture "${TARGETS[@]}" \
 	--min-confidence=1 \
 	--sort-by-size
 
